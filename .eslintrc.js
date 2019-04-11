9f9557defca2c6a5ba193dbdbaa007e40b95b5d7
Anthony Nichols <hi@anthonynichols.me>
2019-04-11 11:14:32 -0500

    Update a few more things

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true
+    }
+  },
+  "plugins": [
+      "babel", // NOTE: Is this needed with the parser set to "babel-eslint"?
+  ]
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
